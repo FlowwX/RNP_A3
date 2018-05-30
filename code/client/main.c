cd7245a774c957fa4ad8b32fa0d690ba62145f45
Florian Heuer <florian.heuer@haw-hamburg.de>
2018-05-30 16:41:24 +0200

    make connection ip version independent

--- code/client/main.c
+++ code/client/main.c
@@ -1,31 +1,33 @@
 /*
     C ECHO client example using sockets
 */
-#include<stdio.h> //printf
-#include<string.h>    //strlen
-#include<sys/socket.h>    //socket
-#include<arpa/inet.h> //inet_addr
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
  
 int main(int argc , char *argv[])
 {
-    int sock;
-    struct sockaddr_in server;
-    char message[1000] , server_reply[2000];
-     
-    //Create socket
-    sock = socket(AF_INET , SOCK_STREAM , 0);
-    if (sock == -1)
-    {
-        printf("Could not create socket");
-    }
-    puts("Socket created");
-     
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");
-    server.sin_family = AF_INET;
-    server.sin_port = htons( 8888 );
+    	int sock;
+  	char message[1000] , server_reply[2000];
+     	struct addrinfo hints, *res;
+
+    	memset( &hints, 0, sizeof(hints) );	
+	
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+
+	getaddrinfo(argv[1], argv[2], &hints, &res);
+
+	sock = socket( res->ai_family, res->ai_socktype, res->ai_protocol );
+
+	printf("%s - %s", argv[1], argv[2]);
  
     //Connect to remote server
-    if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
+    if (connect(sock, res->ai_addr, res->ai_addrlen) )
     {
         perror("connect failed. Error");
         return 1;
